e9884df3ccaca4956888d5d06d3061137e136255
Sean Kermes <skermes@gmail.com>
2018-11-18 13:15:39 -0500

    Bootstrap script and clj runner.

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -1,6 +1,12 @@
-(ns aoc.core)
+(ns aoc.core
+  (:require [clojure.string :as string]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn day-no-zeroes [day-str]
+  (string/replace day-str "0" ""))
+
+(defn -main [year day]
+  (let [puzzle-ns (str "aoc.puzzle-" year "-" (day-no-zeroes day))]
+    (require [(symbol puzzle-ns) :as 'puzzle])
+    (println (eval (list (symbol "puzzle/part-one"))))
+    (println)
+    (println (eval (list (symbol "puzzle/part-two"))))))
---
+++ src/aoc/puzzle_2017_1.clj
@@ -0,0 +1,11 @@
+(ns aoc.puzzle-2017-1
+  (:require [clojure.string :as string]))
+
+(defn input [] (slurp "inputs/2017_1.txt"))
+(defn input-lines [] (string/split-lines (input)))
+
+(defn part-one []
+  "2017 day 1 Part One Stub")
+
+(defn part-two []
+  "2017 day 1 Part Two Stub")
