c1a7df56a51b166ed3c36a60255ee6601ae20dac
Sean Kermes <skermes@gmail.com>
2018-12-07 08:06:33 -0500

    Stub for day 6.
    Had other stuff going on, i'll come back to it.

---
+++ src/aoc/puzzle_2018_6.clj
@@ -0,0 +1,11 @@
+(ns aoc.puzzle-2018-6
+  (:require [clojure.string :as string]))
+
+(defn input [] (string/trim (slurp "inputs/2018_6.txt")))
+(defn input-lines [] (string/split-lines (input)))
+
+(defn part-one []
+  nil)
+
+(defn part-two []
+  nil)
